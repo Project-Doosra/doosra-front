c8f88f61bb1e1b8f92350fca52a8542d9b724118
Abhishek Sharma <abhishek605404@gmail.com>
2025-02-23 19:37:18 +0530

    add: footer, nav

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -9,7 +9,6 @@ import TrendingPlayers from "./components/home/TrendingPlayers";
 import AdditionalLinks from "./components/home/AdditionalLinks";
 import Footer from "./components/layout/Footer";
 
-
 export default function Home() {
   return (
     <main className="min-h-screen bg-[#F5F9FC] text-[#1E1E1E]">
@@ -20,27 +19,33 @@ export default function Home() {
         <Container className="hidden md:flex flex-col">
           <div className="flex flex-row justify-center items-center">
             <div className="flex flex-col items-center gap-3">
-            <SearchBar type="full" placeholder="Search players, predictions, teams, etc." />
-            <TrendingPlayers/>
+              <SearchBar
+                type="full"
+                placeholder="Search players, predictions, teams, etc."
+              />
+              <TrendingPlayers />
             </div>
             <div>
-            <AdditionalLinks alignment="column"/>
+              <AdditionalLinks alignment="column" />
             </div>
           </div>
-        <TopPicks />
+          <TopPicks />
         </Container>
         {/* Container for mobile */}
         <Container className="flex md:hidden flex-col">
-        <SearchBar type="full" placeholder="Search players, predictions, teams, etc." />
-        <TrendingPlayers />
-        <TopPicks />
-        <AdditionalLinks alignment="row"/>
+          <SearchBar
+            type="full"
+            placeholder="Search players, predictions, teams, etc."
+          />
+          <TrendingPlayers />
+          <TopPicks />
+          <AdditionalLinks alignment="row" />
         </Container>
       </div>
-      <Footer/>
+      <Footer />
       <div className="md:hidden fixed bottom-0 w-full">
-      <BottomNavigation />
+        <BottomNavigation />
       </div>
     </main>
-  )
+  );
 }
